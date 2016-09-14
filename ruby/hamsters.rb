512b2191d1f65545e01d51787951781e33ec4912
alealejandro <alex.m.4155@gmail.com>
2016-09-14 14:44:51 -0400

    Modify input validation & choose most suitable data type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,20 +1,25 @@
 puts "What's the hamster's name?"
 name = gets.chomp
-name = name.capitalize
+name = name.split.map(&:capitalize).join(' ')
+# split.map(&:capitalize).join(' ')
+# pretty much runs an each, do on each of the names, split by a space
+# the names are then capitalized, and joined together with a space
+# https://stackoverflow.com/questions/13520162/ruby-capitalize-every-word-first-letter
 
 puts ""
 valid_input = false
 until valid_input
 	puts "How loud is the hamster (1-10)?."
 	volume = gets.chomp
-	volume = volume.to_i
-	if volume > 0 && volume < 11
+	volume = volume.to_f
+	if volume > 0 && volume <= 10
 		valid_input = true
 	else
 		puts ""
 		puts "I didn't understand you."
 	end
 end
+# Valid input only from  0.0 - 10.0
 
 puts ""
 puts "What is its fur color?"
@@ -38,20 +43,32 @@ until valid_input
 		puts "I didn't understand you."
 	end
 end
-good = good.to_s.capitalize
+# Valid input only y/n
+# Output is capitalized boolean.to_s
 
 puts ""
-puts "What is its age?"
-age = gets.chomp
-if age == ""
-	age = nil
-else
-	age = age.to_i
+valid_input = false
+until valid_input
+	puts "What is its age?"
+	age = gets.chomp
+	if age == ""
+		age = nil
+		valid_input = true
+	elsif age.to_f > 0 
+		age = age.to_f
+		valid_input = true
+	else
+		puts ""
+		puts "I didn't understand you."
+	end
 end
+# Takes "" as input > nil
+# Takes any number > 0 as valid (no hamster will be 0.0 years old)
+# Has to be > 0 because .to_i / .to_f both return 0 / 0.0 even if gibberish
 
 puts ""
-puts "Name:             #{name}"
-puts "Volume:           #{volume}"
-puts "Color:            #{color}"
-puts "Good Candidate:   #{good}"
-puts "Estimated Age:    #{age}"
+puts "             Name:     #{name}"
+puts "           Volume:     #{volume}"
+puts "            Color:     #{color}"
+puts "   Good Candidate:     #{good.to_s.capitalize}"
+puts "    Estimated Age:     #{age}"
