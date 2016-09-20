d2ec71f0dbf19954402d4711f2a10a1a43218a84
alealejandro <alex.m.4155@gmail.com>
2016-09-19 22:59:14 -0400

    Create pseudocode for hashes interior designer assignment 5.2

---
+++ ruby/hashes.rb
@@ -0,0 +1,26 @@
+=begin
+Create empty hash called client_details
+
+Ask for client's name (str)
+store input in :name
+Ask for age (str.to_i)
+store in :age
+Ask for number of children (int)
+store in :num_child
+Ask for decor theme (str)
+store in :decor
+
+p client_details
+
+Ask if they need to update a value
+if none 
+	skip
+elsif name
+	- Convert string to symbol & apply changes there
+	change :name
+elsif age 
+	change...
+...
+end
+=end
+
