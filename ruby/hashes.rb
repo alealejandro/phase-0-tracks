228c8c96ec8e30d393e3419a94f4ba88433e73d6
alealejandro <alex.m.4155@gmail.com>
2016-09-20 14:38:14 -0400

    Create another input (children age), change output, add some input validation.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,8 +1,146 @@
+client_details = {}
+alpha_regex = /[[:alpha:]]+/
+# age_regex = /[[:digit:]]+/
+
+puts "\nPlease enter the following details of your client:"
+
+# Name
+valid = false
+until valid
+	puts "\nWhat is the client's name?"
+	print "\n> "
+	client_details[:name] = gets.chomp
+	if client_details[:name].match(alpha_regex) 
+	# is a string, but only doing one character - how to match all characters via iteration? map as array?
+	# scan?
+	# name.split.map(&:capitalize).join(' ')} (something similar)?
+		valid = true
+	else
+		puts "\nInvalid, please try again."
+	end
+end
+
+# Age
+valid = false
+until valid
+	puts "\nWhat is the client's age?"
+	print "\n> "
+	client_details[:age] = gets.chomp.to_i
+	#if client_details[:age].match(age_regex) # is a digit
+	if client_details[:age] > 0
+		valid = true
+	else
+		puts "\nInvalid, please try again."
+	end
+end
+
+# Number of children & Age of children
+valid = false
+until valid
+	puts "\nHow many children does the client have?"
+	print "\n> "
+	client_details[:num_children] = gets.chomp.to_i
+	# to_i defaults to 0 if no number specified, so will always be valid...
+	if client_details[:num_children] >= 0
+		valid = true
+		puts "\nHow old are they?"
+		client_details[:age_children] = []
+		i = 1
+		until i > client_details[:num_children]
+			print"\n> "
+			client_details[:age_children] << gets.chomp.to_i
+			i += 1
+		end
+	else
+		puts "\nInvalid, please try again."
+	end
+end
+
+# Decor Theme
+puts "\nWhat is the client's preferrable decor theme?"
+print "\n> "
+client_details[:decor] = gets.chomp
+
+# Output
+puts "\nName:                #{client_details[:name]}
+Age:                 #{client_details[:age]}
+Number of Children:  #{client_details[:num_children]}
+Age of Children:     #{client_details[:age_children]}
+Decor Theme:         #{client_details[:decor]}"
+# Store into variable?
+
+# Update a key?
+puts "\nWould you like to update any details (y/n)?"
+print "\n> "
+response = gets.chomp.downcase
+if response == "n"
+	puts "\nHere are your client detials:"
+	puts "\nName:                #{client_details[:name]}
+Age:                 #{client_details[:age]}
+Number of Children:  #{client_details[:num_children]}
+Age of Children:     #{client_details[:age_children]}
+Decor Theme:         #{client_details[:decor]}"
+
+elsif response == "y"
+	puts "\nPlease choose which field to update:\nname | age | num_children | age_children | decor"
+	print "\n> "
+	field = gets.chomp.downcase
+	key = field.to_sym
+	if client_details.has_key?(key)
+		puts "\nWhat would you like to update their #{field} to?"
+		print "\n> "
+		case key 
+			when :name
+				update = gets.chomp
+				client_details[:name] = update
+			when :age 
+				update = gets.chomp.to_i
+				client_details[:age] = update
+			# :num_children & age_children have repeatable code
+			when :num_children
+				update = gets.chomp.to_i
+				client_details[:num_children] = update
+
+		  	puts "\nPlease update their ages as well:"
+		  	client_details[:age_children] = []
+		  	i = 1
+		  	until i > client_details[:num_children]
+		  		print"\n> "
+		  		client_details[:age_children] << gets.chomp.to_i
+		  		i += 1
+		  	end
+	  	when :age_children
+			key == :age_children
+			client_details[:age_children] = []
+			i = 1
+			until i > client_details[:num_children]
+				print"\n> "
+				client_details[:age_children] << gets.chomp.to_i
+				i += 1
+			end
+			when :decor
+			update = gets.chomp
+			client_details[:decor] = update
+	  end
+	else
+		puts "\nSorry, that's not a valid field."
+	end
+	puts "\nHere are the updated client details:"
+	puts "\nName:                #{client_details[:name]}
+Age:                 #{client_details[:age]}
+Number of Children:  #{client_details[:num_children]}
+Age of Children:     #{client_details[:age_children]}
+Decor Theme:         #{client_details[:decor]}"
+else
+	puts "\nSorry, that's not a valid field."
+end
+
+
 =begin
 Create empty hash called client_details
 
 Ask for client's name (str)
-store input in :name
+if input is a string of alphabet chars, store input in :name
 Ask for age (str.to_i)
 store in :age
 Ask for number of children (int)
@@ -23,73 +161,3 @@ elsif age
 ...
 end
 =end
-
-client_details = {}
-
-puts "\nPlease enter the following details of your client:"
-puts "\nWhat is the client's name?"
-print "\n> "
-client_details[:name] = gets.chomp
-
-puts "\nWhat is the client's age?"
-print "\n> "
-client_details[:age] = gets.chomp.to_i
-
-puts "\nHow many children does the client have?"
-print "\n> "
-client_details[:num_children] = gets.chomp.to_i
-
-puts "\nWhat is the client's preferrable decor theme?"
-print "\n> "
-client_details[:decor] = gets.chomp
-
-puts "\n #{client_details}"
-
-puts "\nWould you like to update any details (y/n)?"
-print "\n> "
-response = gets.chomp.downcase
-if response == "n"
-	puts "Great."
-else
-	puts "\nPlease choose which field to update:\n   name | age | num_children | decor"
-	print "\n> "
-	field = gets.chomp.downcase
-	if field == "name"
-		key = field.to_sym
-		puts "What would you like to update the #{field} to?"
-		print "\n> "
-		update = gets.chomp
-	  client_details[key] = update
-		# select :name 
-		# update value
-	elsif field == "age"
-		key = field.to_sym
-		puts "What would you like to update the #{field} to?"
-		print "\n> "
-		update = gets.chomp.to_i
-		client_details[key] = update
-	elsif field == "num_children"
-		key = field.to_sym
-		puts "What would you like to update the #{field} to?"
-		print "\n> "
-		update = gets.chomp.to_i
-		client_details[key] = update
-	elsif field == "decor"
-		key = field.to_sym
-		puts "What would you like to update the #{field} to?"
-		print "\n> "
-		update = gets.chomp
-		client_details[key] = update
-	else
-		puts "\nSorry, that's not a valid field."
-	end
-end
-
-puts "\nHere are the updated client details: \n\n#{client_details}"
-
-
-
-
-
-
-
