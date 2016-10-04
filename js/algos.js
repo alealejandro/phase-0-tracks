655c10c3e8bbb62a4e0f7743c6c9f35aa1af8b21
alealejandro <alex.m.4155@gmail.com>
2016-10-04 15:28:32 -0400

    Create simpler bubbleSort for numbers array to test, loops through only one time - need to keep looping

--- js/algos.js
+++ js/algos.js
@@ -3,7 +3,7 @@
   // Function that takes an array of words/phrases & returns the longest word/phrase in the array
     // Input: Bunch of words
     // Steps: 
-      // Initial
+      // Initial (Bubble sort)
         // for each word in the array,
 	        // Measure the length of each word supplied by the array
 	        // Store length of word in new array
@@ -20,14 +20,65 @@
           // 3. Quick sort the right half
 
     // Output: Longest word
+
+// Analogy using numbers so more basic:
+  // For each item in array
+    // Starting with first item, 
+    // Compare item at index with item at next index
+      // If item at next index is greater
+        // Don't flip
+      // If item at next index is smaller
+        // Flip
+    // After comparison,
+      // Rechoose index as next index
+      // Compare again
+      // Loop until at final index
+
+var ar = [1, 3, 2, 4, 8, 0];
+
+var bubbleSort = function(array) {
+  for (i = 0; i < array.length; i++) {
+  	// for each item in array,
+  	// compare to next item
+  	if (array[i] < array[i+1]) {
+  		continue;
+  		// go to next iteration, next index
+  	}
+  	else if (array[i] > array[i+1]) {
+  		temp = array[i]; // store bigger value
+  		array[i] = array[i+1]; // make smaller index have smaller value
+  		array[i+1] = temp; // make bigger index have bigger value
+  	}
+  }
+  return array;
+}
+
+console.log(bubbleSort(ar));
+
+
+/*
 var array1 = ['a', 'abc', 'abcd', 'ab'];
 var array2 = []
 
 var find_largest = function(array) {
 	for (i=0; i < array.length; i++) { // for each word in array
-    array2.push(array[i].length);
+    array2.push(array[i].length);  // store word length in new array
+  }
+  for (i=0; i < array2.length; i++) { 
+  // for each length in new array
+  // Compare with next index
+    // If next index < current index
+      // Switch positions
+      // Next iteration
+    // If next index > current index
+      // Maintain positions
+      // Skip to next iteration so that i = current i + 1
+    if (array2[i] > array2[i+1])
+
+
   }
   return array2;
-}
+};
 
 console.log(find_largest(array1));
+*/
