ba3f4492ac4352f2cc27acc3a8d58ec5e1e92469
alealejandro <alex.m.4155@gmail.com>
2016-10-21 12:18:38 -0700

    Create release 1 & 2

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -6,6 +6,8 @@ console.log(h1s);
 var mainHeader = h1s[0];
 console.log(mainHeader);
 
+////// Release 1 //////
+
 /* My first approach
 function timerFade(element) {
 	for (i = 0; i < 1; i += 0.1) {
@@ -38,18 +40,28 @@ function fadeIn(element) {
 fadeIn(mainHeader);
 
 /* 
-EXTERNAL & INNER COUNTERS needed ?
-opacity = 0
-set .style.opacity == opacity
-based on setTimeout milliseconds,
-run fadeIn
-until .style.opacity == 1;
+	EXTERNAL & INNER COUNTERS needed ?
+	opacity = 0
+	set .style.opacity == opacity
+	based on setTimeout milliseconds,
+	run fadeIn
+	until .style.opacity == 1;
 
-https://stackoverflow.com/questions/23259986/how-to-create-fadein-effect-in-javascript-using-for-loop-and-anonymous-function/23284208
+	https://stackoverflow.com/questions/23259986/how-to-create-fadein-effect-in-javascript-using-for-loop-and-anonymous-function/23284208
 
-https://stackoverflow.com/questions/1776687/settimeout-inside-for-loop
+	https://stackoverflow.com/questions/1776687/settimeout-inside-for-loop
 
-https://duckduckgo.com/?q=settimeout+javascript+for+loop+fade+in&atb=v33-2__&ia=qa
+	https://duckduckgo.com/?q=settimeout+javascript+for+loop+fade+in&atb=v33-2__&ia=qa
 
-http://www.w3schools.com/js/js_timing.asp
+	http://www.w3schools.com/js/js_timing.asp
 */
+
+////// Release 2 //////
+
+// http://www.w3schools.com/jsref/event_onscroll.asp
+
+function logToConsole() {
+	console.log("Running release 2");
+};
+
+document.addEventListener("scroll", logToConsole);
